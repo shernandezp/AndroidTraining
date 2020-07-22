22e9d6629b971ed3152ca60e2d28b7703766c929
CommandTrack - C2LS <soporte@c2ls.co>
2020-07-22 01:54:04 -0400

    Added sensors view

--- app/src/main/java/com/example/mycanada/DataAccess.java
+++ app/src/main/java/com/example/mycanada/DataAccess.java
@@ -2,12 +2,15 @@ package com.example.mycanada;
 
 import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
 public class DataAccess extends SQLiteOpenHelper {
     SQLiteDatabase sqLiteDatabase;
+
     public DataAccess(Context context) {
+        //TODO get database name from configuration file
         super(context, "mycanada.db", null, 1);
         sqLiteDatabase = getWritableDatabase();
     }
@@ -25,5 +28,19 @@ public class DataAccess extends SQLiteOpenHelper {
 
     public void insert(String table, ContentValues values) {
         sqLiteDatabase.insert(table, null, values);
+    }
+
+    public void update(String table, ContentValues values, String whereClause, String[] whereArgs) {
+        sqLiteDatabase.update(table, values, whereClause, null);
+    }
+
+    public void delete(String table, String whereClause, String[] whereArgs) {
+        sqLiteDatabase.delete(table, whereClause, null);
+    }
+
+    public Cursor get(String table, String[] columns, String selection, String[] selectionArgs, String groupBy, String having, String orderBy) {
+        Cursor c = sqLiteDatabase.query(table, columns, selection, selectionArgs, groupBy, having, orderBy);
+        c.moveToFirst();
+        return c;
     }
 }
--- app/src/main/java/com/example/mycanada/User.java
+++ app/src/main/java/com/example/mycanada/User.java
@@ -2,9 +2,13 @@ package com.example.mycanada;
 
 import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
+
+import java.util.ArrayList;
 
 public class User extends DataAccess {
 
+    private Context context;
     private String mobile;
     private String birth;
     private int gender;
@@ -12,6 +16,7 @@ public class User extends DataAccess {
 
     public User(Context context, String mobile, String birth, int gender, int qualification) {
         super(context);
+        this.context = context;
         this.mobile = mobile;
         this.birth = birth;
         this.gender = gender;
@@ -25,5 +30,49 @@ public class User extends DataAccess {
         contentValues.put("Gender", this.gender);
         contentValues.put("Qualification", this.qualification);
         insert("user", contentValues);
+    }
+
+    public void UpdateUser() {
+        ContentValues contentValues = new ContentValues();
+        contentValues.put("Birth", this.birth);
+        contentValues.put("Gender", this.gender);
+        contentValues.put("Qualification", this.qualification);
+        update("user", contentValues, "Mobile = ?", new String[]{this.mobile});
+    }
+
+    public void DeleteUser() {
+        delete("user", "Mobile = ?", new String[]{this.mobile});
+    }
+
+    public User GetUser() {
+        String result = "";
+        Cursor c = get("user", null, "Mobile=?", new String[]{this.mobile}, null, null, null);
+        c.moveToFirst();
+        if (c.getCount() < 1) {
+            return null;
+        }
+        User user = new User(context,
+                c.getString(c.getColumnIndex("Mobile")),
+                c.getString(c.getColumnIndex("Birth")),
+                c.getInt(c.getColumnIndex("Gender")),
+                c.getInt(c.getColumnIndex("Qualification")));
+        return user;
+    }
+
+    public ArrayList<User> GetUsers() {
+        ArrayList users = new ArrayList<User>();
+        Cursor c = get("user", null, "Mobile=?", new String[]{this.mobile}, null, null, null);
+        c.moveToFirst();
+        if (c.getCount() > 0) {
+            do {
+                User user = new User(context,
+                        c.getString(c.getColumnIndex("Mobile")),
+                        c.getString(c.getColumnIndex("Birth")),
+                        c.getInt(c.getColumnIndex("Gender")),
+                        c.getInt(c.getColumnIndex("Qualification")));
+                users.add(user);
+            } while (c.moveToNext());
+        }
+        return users;
     }
 }
